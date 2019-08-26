84e07eb4dce18aa28c54e23b5274ee98db5d99af
Anthony Lee <awllee@gmail.com>
2019-08-26 11:25:53 +0100

    add plot code

--- paper/simpleATPlot.jl
+++ paper/simpleATPlot.jl
@@ -4,9 +4,17 @@ using DataFrames
 
 @load "paper/box_all.jld2" box_all_df
 @load "paper/box_at.jld2" box_at_df
+@load "paper/lg_all.jld2" lg_all_df
 
 @df box_at_df scatter(:n, :mean)
 p = @df box_all_df plot(:n, :mean, group=:type, marker=4,
-  xlabel="T", ylabel="mean coupling time", xticks=200:200:1600, size=(450,250))
+  xlabel="T", ylabel="mean coupling time", xticks=200:200:1600, size=(450,250),
+  legend=:topleft)
 
 savefig(p, "paper/simpleComparisonATASBS.pdf")
+
+p = @df lg_all_df plot(:n, :mean, group=:type, marker=4,
+  xlabel="T", ylabel="mean coupling time", xticks=200:200:1600, size=(450,250),
+  legend=:topleft)
+
+savefig(p, "paper/simpleComparison_lg_ATASBS.pdf")
